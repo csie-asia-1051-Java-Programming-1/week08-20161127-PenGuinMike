e8d43ccfc284670f003b63fcb8a994d0efe53f0d
PenGuinMike <104021074@live.asia.edu.tw>
2016-11-26 11:59:32 +0800

    十進制轉二進制:遞回

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -2,12 +2,23 @@ package hw;
 /*
  * Topic: 請寫一個 遞迴函式)進行十進制數字轉換成二進制數字，例如十進制的 13 會輸出二進制的 1101
  * Date: 2016/11/21
- * Author: 1050210XX 周永振老師
+ * Author: 104021074 楊永聖
  */
+import java.util.*;
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int a = scn.nextInt();
+		System.out.println(fun(a));
+	}
+	public static long fun(int a){
+		if(a>1){
+			return fun(a/2)*10+a%2;
+		}else{
+			return a%2;
+		}
 
 	}
 
