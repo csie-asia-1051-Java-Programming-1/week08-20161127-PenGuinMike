c8308fc4585cf74e36a7fc9648de0d80e5fbc2e8
PenGuinMike <104021074@live.asia.edu.tw>
2016-11-26 11:58:01 +0800

    類別:計算機

--- src/ex/ex04.java
+++ src/ex/ex04.java
@@ -4,12 +4,54 @@
  * Date: 2016/11/21
  * Author: 104021074 楊永聖
  */
-
+import java.util.*;
 public class ex04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		computer cop = new computer();
+		float a = scn.nextFloat();
+		char b = scn.next().charAt(0);
+		float c = 0;
+		if(b=='+'||b=='-'||b=='*'||b=='/'){
+			c=scn.nextFloat();
+		}System.out.println(computer.fun1(a,b,c));
 	}
+	 
 
+}
+class computer{
+	
+	
+	public static float fun1(float a,char b,float c){
+		float x = 0;
+		switch(b){
+		case '+':
+			x=a+c;
+			break;
+		case '-':
+			x=a-c;
+			break;
+		case '*':
+			x=a*c;
+			break;
+		case '/':
+			x=a/c;
+			break;
+		case '^':
+			x=(float) Math.pow(a, 2);
+			break;
+		case '3':
+			x=(float) Math.pow(a, 3);
+			break;
+		case 'L':
+			x=(float) Math.sqrt(a);
+			break;
+		
+		
+		}
+		
+		return x;
+	}
 }
